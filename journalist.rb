34b6d9a8057fe70560d591d9073c45f7846d5fa9
nanacy <nancy.cassand@gmail.com>
2018-07-05 21:09:11 +0200

    ajout journalist + crypto exo

---
+++ journalist.rb
@@ -0,0 +1,159 @@
+liste_journalist = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+##########################
+# COMBIEN DE JOURNALISTE #
+##########################
+def taille(liste)
+	return liste.length ##379 #size
+end
+
+####################
+# CONTIENT CHIFFRE #
+####################
+def contient_nb_de(liste)
+	result = liste.select {|item| item.match(/\d/)}
+	return result.size
+end
+# 	# listeChiffre = ("0".."9")
+# 	listeChiffre = ["0","1","2","3","4","5","6","7","8","9"]
+# 	nb_pseudo = 0
+# 	i=0
+
+# 	liste.each do |pseudo| #passe de pseudo en pseudo
+# 		pseudo.length.times do |lettre| #tourne sur chaque lettre du pseudo
+# 			while i<listeChiffre.length #tant qu'on ne dépasse pas la liste de chiffre
+# 				if listeChiffre[i]==pseudo[lettre] #boucle sur numero -> si num=lettre
+# 					nb_pseudo+=1		 #nb de pseudo augmente de 1 
+# 					break	# casse la boucle pour ne pas rester dans le même mot 
+# 				end 	#passe sur le numero suivant
+# 				i+=1
+# 			end
+# 			i=0
+# 		end
+# 	end
+# 	return nb_pseudo
+# end
+
+#################
+# CONTIENT AUDE #
+#################
+def contient_mot(liste, mot)
+	rep = liste.select{|a| a.include?(*mot)}
+	return rep.size
+end
+
+#####################
+# COMMENCE AVEC MAJ #
+#####################
+def begin_maj(liste)
+	nb_pseudo=0
+	maj=("A".."Z")
+
+	liste.each do |pseudo|
+		maj.each do |majuscule|
+			if pseudo[1] == majuscule
+				nb_pseudo += 1
+			end
+		end
+	end
+	return nb_pseudo
+end
+# Fonction 2 #
+def begin_maj2(liste, mot)
+	liste_mot=liste.select{|a| a.start_with?(*mot)}
+	return liste_mot.size
+end
+
+###################
+# CONTIENNENT MAJ #
+###################
+def have_maj(liste)
+	num_pseudo = 0
+	maj=("A".."Z")
+
+	liste.each do |pseudo| #passe de pseudo en pseudo
+		i=0
+		while i<pseudo.length
+			if maj.include?(pseudo[i])
+				num_pseudo+=1
+				break
+			end
+			i+=1
+		end
+	end
+	return num_pseudo
+end
+
+
+####################
+# UNDERSCORE ( _ ) #
+####################
+def underscore_count(liste)
+	nb_underscore = liste.select{|a| a.include? "_"}
+	return nb_underscore.size
+end
+
+##################################
+# TRIER LISTE ORDRE ALPHABETIQUE #
+##################################
+def trie(liste)
+	liste.sort do |persA, persB|
+		persA<=>persB
+	end
+end
+
+#--------------------------------------------#
+#               APPEL FONCTIONS 		     #
+#--------------------------------------------#
+
+##########################
+# COMBIEN DE JOURNALISTE #
+##########################
+puts "Taille de la liste : #{taille(liste_journalist)}"
+puts "-"*20
+
+####################
+# CONTIENT CHIFFRE #
+####################
+puts "Nombre de pseudo contenant un numéro : #{contient_nb_de(liste_journalist)}"
+puts "-"*20
+
+#################
+# CONTIENT AUDE #
+#################
+newlistejournalist = liste_journalist.map(&:downcase)
+trescherAude="aude"
+puts "Nombre de pseudo avec aude : #{contient_mot(newlistejournalist, trescherAude)}"
+
+puts"-"*20
+
+#####################
+# COMMENCE AVEC MAJ #
+#####################
+puts "Fonction1 : Nombre de pseudo commencant par une majuscule : #{begin_maj(liste_journalist)}"
+# avec la fonction 2
+liste_lettre=('@A'...'@Z')
+puts "Fonction2 : Nombre de pseudo commencant par une majuscule : #{begin_maj2(liste_journalist, liste_lettre)}"
+puts "-"*20
+
+###################
+# CONTIENNENT MAJ #
+###################
+puts "Nombre de pseudo contenant une majuscule : #{have_maj(liste_journalist)}"
+puts "-"*20
+
+####################
+# UNDERSCORE ( _ ) #
+####################
+puts "Nombre de underscore : #{underscore_count(liste_journalist)}"
+puts"-"*20
+
+##################################
+# TRIER LISTE ORDRE ALPHABETIQUE #
+##################################
+print trie(liste_journalist)
+puts"-"*20
+
+
+
+
